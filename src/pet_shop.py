10e13288b75a5e5f602d0c44cafb5f670dc6f5e2
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-02-25 09:30:40 +0000

    homework first 6 or 7 questions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,32 @@
+import pdb
+
 # WRITE YOUR FUNCTIONS HERE
+
+
+#  pet shop name
+def get_pet_shop_name(dictionary):
+    return dictionary["name"]
+
+def get_total_cash(dictionary):
+    current_cash = dictionary["admin"]["total_cash"]
+    return current_cash
+
+def add_or_remove_cash(dictionary, amount):
+    new_cash = dictionary["admin"]["total_cash"] = get_total_cash(dictionary) + amount
+    return new_cash
+
+def get_pets_sold(dictionary):
+    pets_sold = dictionary["admin"]["pets_sold"]
+    return pets_sold
+
+def pets_sold(dictionary):
+    pets_we_sold = get_pets_sold(dictionary)
+    return pets_we_sold
+
+def increase_pets_sold(dictionary, amount):
+     new_total = dictionary["admin"]["pets_sold"] = amount
+     return new_total
+    
+def get_stock_count(dictionary):
+    stock_count = len(dictionary["pets"])
+    return stock_count
---
+++ src/test.py
@@ -0,0 +1,64 @@
+pet_shop = {
+            "pets": [
+                {
+                    "name": "Sir Percy",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "King Bagdemagus",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "Sir Lancelot",
+                    "pet_type": "dog",
+                    "breed": "Pomsky",
+                    "price": 1000,
+                },
+                {
+                    "name": "Arthur",
+                    "pet_type": "dog",
+                    "breed": "Husky",
+                    "price": 900,
+                },
+                {
+                    "name": "Tristan",
+                    "pet_type": "cat",
+                    "breed": "Basset Hound",
+                    "price": 800,
+                },
+                {
+                    "name": "Merlin",
+                    "pet_type": "cat",
+                    "breed": "Egyptian Mau",
+                    "price": 1500,
+                }
+            ],
+            "admin": {
+                "total_cash": 1000,
+                "pets_sold": 0,
+            },
+            "name": "Camelot of Pets"
+        }
+
+def get_pet_shop_name(dictionary):
+    return dictionary["name"]
+
+def get_total_cash(dictionary):
+    return dictionary["admin"]["total_cash"]
+
+
+
+def add_or_remove_cash(dictionary, amount):
+    current_cash = get_total_cash(dictionary)
+    cash = current_cash + amount
+    return cash
+
+def add_or_remove_cash_add(dictionary, amount):
+    cash = add_or_remove_cash(dictionary, amount)
+    return cash
+
+print(add_or_remove_cash_add(pet_shop, 10))
