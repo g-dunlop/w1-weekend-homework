723668f18bead18e58f79fff98a2509c5f857aab
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-02-26 07:46:09 +0000

    finished mvp

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -30,3 +30,45 @@ def increase_pets_sold(dictionary, amount):
 def get_stock_count(dictionary):
     stock_count = len(dictionary["pets"])
     return stock_count
+
+def get_pets_by_breed(dictionary, breed):
+        pets = []
+        for pet in dictionary["pets"]:
+            if pet["breed"] == breed:
+                pets.append(pet)
+            
+        if len(pets) == 0:
+            return pets
+        else:
+            return pets
+        
+def find_pet_by_name(dictionary, name):
+    for pet in dictionary["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def remove_pet_by_name(dictionary, name):
+    index = 0
+    for pet in dictionary["pets"]:
+        if pet["name"] == name:
+            del(dictionary["pets"][index])
+        index += 1
+
+def add_pet_to_stock(dictionary, new_pet):
+    dictionary["pets"].append(new_pet)
+    return dictionary["pets"]
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    existing_cash = customer["cash"]
+    customer["cash"] = existing_cash - amount
+    
+    
+def get_customer_pet_count(customer):
+    pets = customer["pets"]
+    return len(pets)
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
--- src/test.py
+++ src/test.py
@@ -1,64 +1,127 @@
-pet_shop = {
-            "pets": [
-                {
-                    "name": "Sir Percy",
-                    "pet_type": "cat",
-                    "breed": "British Shorthair",
-                    "price": 500
-                },
-                {
-                    "name": "King Bagdemagus",
-                    "pet_type": "cat",
-                    "breed": "British Shorthair",
-                    "price": 500
-                },
-                {
-                    "name": "Sir Lancelot",
-                    "pet_type": "dog",
-                    "breed": "Pomsky",
-                    "price": 1000,
-                },
-                {
-                    "name": "Arthur",
-                    "pet_type": "dog",
-                    "breed": "Husky",
-                    "price": 900,
-                },
-                {
-                    "name": "Tristan",
-                    "pet_type": "cat",
-                    "breed": "Basset Hound",
-                    "price": 800,
-                },
-                {
-                    "name": "Merlin",
-                    "pet_type": "cat",
-                    "breed": "Egyptian Mau",
-                    "price": 1500,
-                }
-            ],
-            "admin": {
-                "total_cash": 1000,
-                "pets_sold": 0,
+customers = [
+            {
+                "name": "Alice",
+                "pets": [],
+                "cash": 1000
             },
-            "name": "Camelot of Pets"
-        }
+            {
+                "name": "Bob",
+                "pets": [],
+                "cash": 50
+            },
+            {
+                "name": "Jack",
+                "pets": [],
+                "cash": 100
+            }
+        ]
 
-def get_pet_shop_name(dictionary):
-    return dictionary["name"]
+def get_customer_cash(customer):
+    return customer["cash"]
 
-def get_total_cash(dictionary):
-    return dictionary["admin"]["total_cash"]
+print(get_customer_cash(customers[0]))
+# def get_customer_cash(list):
+#     customer_cash = []
+#     for customer in list:
+#         for i in customer:
+#             if i == "cash":
+#                 customer_cash.append(customer[i])
+#     return customer_cash
+
+# print(get_customer_cash(customers))
+# pet_shop = {
+#             "pets": [
+#                 {
+#                     "name": "Sir Percy",
+#                     "pet_type": "cat",
+#                     "breed": "British Shorthair",
+#                     "price": 500
+#                 },
+#                 {
+#                     "name": "King Bagdemagus",
+#                     "pet_type": "cat",
+#                     "breed": "British Shorthair",
+#                     "price": 500
+#                 },
+#                 {
+#                     "name": "Sir Lancelot",
+#                     "pet_type": "dog",
+#                     "breed": "Pomsky",
+#                     "price": 1000,
+#                 },
+#                 {
+#                     "name": "Arthur",
+#                     "pet_type": "dog",
+#                     "breed": "Husky",
+#                     "price": 900,
+#                 },
+#                 {
+#                     "name": "Tristan",
+#                     "pet_type": "cat",
+#                     "breed": "Basset Hound",
+#                     "price": 800,
+#                 },
+#                 {
+#                     "name": "Merlin",
+#                     "pet_type": "cat",
+#                     "breed": "Egyptian Mau",
+#                     "price": 1500,
+#                 }
+#             ],
+#             "admin": {
+#                 "total_cash": 1000,
+#                 "pets_sold": 0,
+#             },
+#             "name": "Camelot of Pets"
+#         }
+
+
+# def find_pet_by_name(dictionary, name):
+#     for pet in dictionary["pets"]:
+#         if pet["name"] == name:
+#             return pet
+
+# print(find_pet_by_name(pet_shop, "Jeff"))
+
+# def remove_pet_by_name(dictionary, name):
+#     for pet in dictionary["pets"]:
+#         index = 0
+#         if pet["name"] == name:
+#             del(dictionary["pets"][index])
+#         index +=1
+#     find_pet_by_name(dictionary, name)
+
+
+# print(remove_pet_by_name(pet_shop, "Arthur"))
+
+# def get_pets_by_breed(dictionary, breed):
+#     pet_by_breed = []
+#     for pet in dictionary["pets"]:
+#         if pet["breed"] == breed:
+#             pet_by_breed.append(pet)
+        
+#     if len(pet_by_breed) == 0:
+#         return pet_by_breed
+#     else:
+#         return pet_by_breed
+        
+# print(get_pets_by_breed(pet_shop, "British Shorthair"))
+
+# def get_pet_shop_name(dictionary):
+#     return dictionary["name"]
+
+# def get_total_cash(dictionary):
+#     return dictionary["admin"]["total_cash"]
 
 
 
-def add_or_remove_cash(dictionary, amount):
-    current_cash = get_total_cash(dictionary)
-    cash = current_cash + amount
-    return cash
+# def add_or_remove_cash(dictionary, amount):
+#     current_cash = get_total_cash(dictionary)
+#     cash = current_cash + amount
+#     return cash
 
-def add_or_remove_cash_add(dictionary, amount):
-    cash = add_or_remove_cash(dictionary, amount)
-    return cash
+# def add_or_remove_cash_add(dictionary, amount):
+#     cash = add_or_remove_cash(dictionary, amount)
+#     return cash
 
-print(add_or_remove_cash_add(pet_shop, 10))
+# print(add_or_remove_cash_add(pet_shop, 10))
