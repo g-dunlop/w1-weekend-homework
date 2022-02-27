8b366c277386f4ce4898ef2eb64cf35c0de28c12
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-02-27 06:45:43 +0000

    just stuck on the last error

--- src/test.py
+++ src/test.py
@@ -16,10 +16,10 @@ customers = [
             }
         ]
 
-def get_customer_cash(customer):
-    return customer["cash"]
+# def get_customer_cash(customer):
+#     return customer["cash"]
 
-print(get_customer_cash(customers[0]))
+# print(get_customer_cash(customers[0]))
 # def get_customer_cash(list):
 #     customer_cash = []
 #     for customer in list:
@@ -29,51 +29,51 @@ print(get_customer_cash(customers[0]))
 #     return customer_cash
 
 # print(get_customer_cash(customers))
-# pet_shop = {
-#             "pets": [
-#                 {
-#                     "name": "Sir Percy",
-#                     "pet_type": "cat",
-#                     "breed": "British Shorthair",
-#                     "price": 500
-#                 },
-#                 {
-#                     "name": "King Bagdemagus",
-#                     "pet_type": "cat",
-#                     "breed": "British Shorthair",
-#                     "price": 500
-#                 },
-#                 {
-#                     "name": "Sir Lancelot",
-#                     "pet_type": "dog",
-#                     "breed": "Pomsky",
-#                     "price": 1000,
-#                 },
-#                 {
-#                     "name": "Arthur",
-#                     "pet_type": "dog",
-#                     "breed": "Husky",
-#                     "price": 900,
-#                 },
-#                 {
-#                     "name": "Tristan",
-#                     "pet_type": "cat",
-#                     "breed": "Basset Hound",
-#                     "price": 800,
-#                 },
-#                 {
-#                     "name": "Merlin",
-#                     "pet_type": "cat",
-#                     "breed": "Egyptian Mau",
-#                     "price": 1500,
-#                 }
-#             ],
-#             "admin": {
-#                 "total_cash": 1000,
-#                 "pets_sold": 0,
-#             },
-#             "name": "Camelot of Pets"
-#         }
+pet_shop = {
+            "pets": [
+                {
+                    "name": "Sir Percy",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "King Bagdemagus",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "Sir Lancelot",
+                    "pet_type": "dog",
+                    "breed": "Pomsky",
+                    "price": 1000,
+                },
+                {
+                    "name": "Arthur",
+                    "pet_type": "dog",
+                    "breed": "Husky",
+                    "price": 900,
+                },
+                {
+                    "name": "Tristan",
+                    "pet_type": "cat",
+                    "breed": "Basset Hound",
+                    "price": 800,
+                },
+                {
+                    "name": "Merlin",
+                    "pet_type": "cat",
+                    "breed": "Egyptian Mau",
+                    "price": 1500,
+                }
+            ],
+            "admin": {
+                "total_cash": 1000,
+                "pets_sold": 0,
+            },
+            "name": "Camelot of Pets"
+        }
 
 
 # def find_pet_by_name(dictionary, name):
@@ -125,3 +125,12 @@ print(get_customer_cash(customers[0]))
 #     return cash
 
 # print(add_or_remove_cash_add(pet_shop, 10))
+
+def add_pet_to_customer(customer, new_pet):
+     updated = customer["pets"]
+     print(updated)
+     updated.append(new_pet)
+     customer["pets"] = updated
+     print(updated)
+
+add_pet_to_customer(customers[0], pet_shop["pets"][2])
--- src/test2.py
+++ src/test2.py
@@ -1,9 +1,74 @@
-import pdb
+customers = [
+            {
+                "name": "Alice",
+                "pets": [],
+                "cash": 1000,
+            },
+            {
+                "name": "Bob",
+                "pets": [],
+                "cash": 50,
+            },
+            {
+                "name": "Jack",
+                "pets": [],
+                "cash": 100,
+            }
+        ]
 
-# WRITE YOUR FUNCTIONS HERE
+new_pet = {
+    "name": "Bors the Younger",
+    "pet_type": "cat",
+    "breed": "Cornish Rex",
+    "price": 100
+}
 
+pet_shop = {
+    "pets": [
+        {
+            "name": "Sir Percy",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500,
+        },
+        {
+            "name": "King Bagdemagus",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500,
+        },
+        {
+            "name": "Sir Lancelot",
+            "pet_type": "dog",
+            "breed": "Pomsky",
+            "price": 1000,
+        },
+        {
+            "name": "Arthur",
+            "pet_type": "dog",
+            "breed": "Husky",
+            "price": 900,
+        },
+        {
+            "name": "Tristan",
+            "pet_type": "cat",
+            "breed": "Basset Hound",
+            "price": 800,
+        },
+        {
+            "name": "Merlin",
+            "pet_type": "cat",
+            "breed": "Egyptian Mau",
+            "price": 1500,
+        }
+    ],
+    "admin": {
+        "total_cash": 1000,
+        "pets_sold": 0,
+    },
+    "name": "Camelot of Pets"
+}
 
-#  pet shop name
 def get_pet_shop_name(dictionary):
     return dictionary["name"]
 
@@ -69,11 +134,12 @@ def remove_customer_cash(customer, amount):
     
     
 def get_customer_pet_count(customer):
-    return len(customer["pets"])
+    pets = customer["pets"]
+    return len(pets)
 
-def add_pet_to_customer(customer, pet):
+def add_pet_to_customer(customer, new_pet):
      updated = customer["pets"]
-     updated.append(pet)
+     updated.append(new_pet)
      customer["pets"] = updated
 
 def customer_can_afford_pet(customer, pet):
@@ -81,6 +147,14 @@ def customer_can_afford_pet(customer, pet):
         return True
     else:
         return False
+
+def increase_pets_sold(dictionary, number):
+     new_total = dictionary["admin"]["pets_sold"] 
+     new_total += number
+     dictionary["admin"]["pets_sold"] = new_total
+     return new_total
+
+
 
 def sell_pet_to_customer(dictionary, pet_name, customer):
     pet = find_pet_by_name(dictionary, pet_name)
@@ -97,3 +171,9 @@ def sell_pet_to_customer(dictionary, pet_name, customer):
             remove_customer_cash(customer, amount)
             add_or_remove_cash(dictionary, amount)
             increase_pets_sold(dictionary, len(customer["pets"]))
+
+sell_pet_to_customer(pet_shop, "Arthur", customers[0])
+print(customers[0])
+print(get_customer_pet_count(customers[0]))
+print(pets_sold(pet_shop))
+print(pet_shop)
